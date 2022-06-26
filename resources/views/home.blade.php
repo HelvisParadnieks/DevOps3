80009e41d406a7a88e4d2e1aafe7f0b5cc08ae01
Helvis <para0006@hz.nl>
2022-06-26 15:03:23 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Hope Page</div>
+                <div class="card-header">Home Page</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,7 +15,7 @@
                     @endif
 
                    <p>
-                        Welcome to my shoping list CRUD website!
+                        Welcome to my shopping list CRUD website!
                    </p>
                 </div>
             </div>
